27e1dcdac32ec04aec5f39e3cf0c087b67ec7e10
vxfontes <nessa1vane@icloud.com>
2022-12-07 09:48:52 -0300

    criando models e adicionando coordenador

---
+++ src/models/Coordenador/Coordenador.java
@@ -0,0 +1,37 @@
+package models.Coordenador;
+
+public class Coordenador implements CoordenadorAcoes {
+
+    
+    // metodos implementados da interface
+    @Override
+    public void adicionarSala() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void exibirSalas() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void editarSalas() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void cadastrarAluno() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void cadastrarProfessor() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void visualizarTurmas() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/models/Coordenador/CoordenadorAcoes.java
@@ -0,0 +1,11 @@
+package models.Coordenador;
+
+public interface CoordenadorAcoes {
+    
+    void adicionarSala();
+    void exibirSalas();
+    void editarSalas();
+    void cadastrarAluno();
+    void cadastrarProfessor();
+    void visualizarTurmas();
+}
