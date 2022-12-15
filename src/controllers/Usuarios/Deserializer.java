26458c53fd04a4312798a617aeb17fa7c92a648b
Aldair Sousa <aldairssousa@tuta.io>
2022-12-15 13:36:56 -0300

    deserializer

--- src/controllers/Usuarios/Deserializer.java
+++ src/controllers/Usuarios/Deserializer.java
@@ -12,37 +12,59 @@ import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import models.Registros.Alunos;
+import models.Registros.Professores;
 import models.Registros.Registro;
 /**
  *
  * @author Unknown
+ * @param <T>
  */
+@SuppressWarnings("unchecked")
 public class Deserializer <T extends Registro> {
     
     public Deserializer(){
         
     }
-    
-    public ArrayList<T> deserializeObject(T objeto) throws FileNotFoundException, IOException{
+
+    public ArrayList<T> deserializeObject(T objeto){
         
-        ArrayList<T> lista = new ArrayList<>();
+        ArrayList<T> lista = new ArrayList<T>();
         
         File directoryPath = new File(objeto.getPath());
         String [] fileNames = directoryPath.list();
-        
+
         for (String fileName : fileNames){
-            FileInputStream file = new FileInputStream(fileName);
-            ObjectInputStream oin = new ObjectInputStream(file);
+            FileInputStream file = null;
             try {
-                lista.add((T) oin.readObject());
-            } catch (ClassNotFoundException ex) {
+                file = new FileInputStream(new File(directoryPath, fileName));
+                ObjectInputStream oin = new ObjectInputStream(file);
+                try {
+                    lista.add((T) oin.readObject());
+                } catch (ClassNotFoundException ex) {
+                    System.out.println("deu ruim aqui, cabeça");
+                }
+            } catch (FileNotFoundException ex) {
                 Logger.getLogger(Deserializer.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Deserializer.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    file.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Deserializer.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
-        
         return lista;
     }
     
-    
+    public static void main(String[] args) throws IOException {
+        Deserializer des = new Deserializer();
+        ArrayList<Professores> alunos = (ArrayList<Professores>) des.deserializeObject(new Professores());
+        for (Professores aluno : alunos){
+            System.out.println(aluno);
+        }
+    }
     
 }
